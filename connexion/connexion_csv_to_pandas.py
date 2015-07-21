f42a510c61d4a2954504ebe1087356dde3bd42c9
Axel Tidemann <axel.tidemann@gmail.com>
2015-07-21 15:52:48 +0200

    Added connexion data reader.

---
+++ connexion/connexion_csv_to_pandas.py
@@ -0,0 +1,17 @@
+'''
+Import CSV data to pandas.
+
+Author: Axel.Tidemann@telenor.com
+'''
+
+
+import sys
+
+import pandas as pd
+
+with pd.HDFStore('data.h5', 'w', complevel=9, complib='blosc') as store:
+    for input_file in sys.argv[1:]:
+        csv = pd.read_csv(input_file, index_col=0, chunksize=50000)
+        for chunk in csv:
+            store.append('connexion', chunk, data_columns=True)
+        print '{} stored in HDF5.'.format(input_file)
