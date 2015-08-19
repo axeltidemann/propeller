1b299b776733c7268199dbf094e0a5174b8b4221
Axel Tidemann <axel.tidemann@gmail.com>
2015-08-19 14:08:26 +0200

    Parallelized corpus analysis code.

--- TACOS/tacos_corpus_analysis.py
+++ TACOS/tacos_corpus_analysis.py
@@ -17,19 +17,13 @@ import ipdb
 
 #logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def frequencies(store):
-    
-    columns = [ 'identifier', 'node', 'alarmtype', 'kpafylke', 'kpakommune', 'fhsseverity', 'severity', \
-                'class', 'fhsproblemarea', 'summary' ]
-    data = []
-    for col in columns:
-        column = store.select_column('tacos', col)
-        unique = len(column.unique())
-        nan = sum(pd.isnull(column))
-        data.append([ unique, 100.0*unique/store.get_storer('tacos').nrows, nan, 100.0*nan/store.get_storer('tacos').nrows ])
-
-    return pd.DataFrame(data, index = columns, columns = ['unique', 'unique_ratio', 'nan', 'nan_ratio'])
-    
+def frequency(column, path):
+    with pd.get_store(path) as store:
+        result = store.select_column('tacos', column)
+        unique = len(result.unique())
+        nan = sum(pd.isnull(result))
+        
+    return [ column, [ unique, 100.0*unique/store.get_storer('tacos').nrows, nan, 100.0*nan/store.get_storer('tacos').nrows ] ]
 
 class DFSentences:
     def __init__(self, store, column):
@@ -47,7 +41,15 @@ def save_word_model(column, path):
         model = gensim.models.Word2Vec(sentences) #, workers=mp.cpu_count())
         model.save('{}.word2vec.{}'.format(path, column))
 
-partial_save = partial(save_word_model, path=sys.argv[1])
+
+partial_freq = partial(frequency, path=sys.argv[1])
 
 pool = mp.Pool()
-pool.map(partial_save, ['identifier', 'node', 'alarmtype',  'fhsproblemara' ])
+columns, data = zip(*pool.map(partial_freq, [ 'identifier', 'node', 'alarmtype', 'kpafylke', 'kpakommune',
+                                              'fhsseverity', 'severity', 'class', 'fhsproblemarea', 'summary' ]))
+print pd.DataFrame(data, index=columns, columns=['unique', 'unique_ratio', 'nan', 'nan_ratio'])
+
+sys.exit()
+    
+partial_save = partial(save_word_model, path=sys.argv[1])
+pool.map(partial_save, ['identifier', 'node', 'alarmtype',  'fhsproblemarea' ])
