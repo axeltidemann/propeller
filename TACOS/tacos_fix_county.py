d46346a81513e0ccc26153f337b2971b8336f55a
Axel Tidemann <axel.tidemann@gmail.com>
2015-08-21 12:58:07 +0200

    Error window calculation.

--- TACOS/tacos_fix_county.py
+++ TACOS/tacos_fix_county.py
@@ -7,19 +7,29 @@ Author: Axel.Tidemann@telenor.com
 '''
 
 import sys
-import multiprocessing as mp
-from functools import partial
-import time
 
 import pandas as pd
 import numpy as np
 
-        
 with pd.get_store(sys.argv[1]) as store:
     # 'identifier' has the fewest NaN values, only ~1e-7
     # 'node' also specifies location, and has only .1% missing values.
-    nodes = np.unique(store.select('tacos', columns=['node', 'kpafylke']))
+    nodes = store.select('tacos', columns=['node', 'kpafylke'])
 
+nodes.replace(['n/a', 'Ukjent'], [np.nan, np.nan], inplace=True)
 # This means: aggregate kpafylke on node, and indicate where there are more than 1 non-NaN entries.
-unique = nodes.groupby(['node']).agg([lambda x: len(np.unique(x[ ~pd.isnull(x) ] )) > 1 ])
-fillers = unique[ unique['kpafylke', '<lambda>'] ]
+manys = nodes.groupby(['node']).agg([lambda x: len(np.unique(x[ ~pd.isnull(x) ] )) > 1])
+# Find nodes where there is exactly one 1 non-NaN entry.
+uniques = nodes.groupby(['node']).agg([lambda x: len(np.unique(x[ ~pd.isnull(x) ] )) == 1])
+
+sums = nodes.groupby(['node']).agg([len])
+real_sums = sums[ uniques['kpafylke', '<lambda>'] ]
+
+print """
+{}% of nodes with kpafylke have more than one kpafylke assigned.
+{}% of unique nodes have exactly one kpafylke assigned.
+Using nodes, we can fill in {}% of the missing kpafylke values.
+""".format(100.*np.mean(manys)/(np.mean(manys)+np.mean(uniques)),
+           100.*np.mean(uniques),
+           100.*np.sum(real_sums)/len(nodes))
+ 
