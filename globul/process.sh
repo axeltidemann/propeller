3c6fe1645fa23db5d990b2fa4e7e0351ec06c5a5
Axel Tidemann <axel.tidemann@gmail.com>
2015-09-02 11:25:23 +0200

    Removed process.sh, preferable to do reading/writing of ggsn/msc data in parallel, and merge afterwards. That will be a lot quicker.

--- globul/process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export GLOBUL=/mnt/data/FromTnBul
-
-rm $GLOBUL/data.h5
-
-#time python globul_metadata_to_pandas.py $GLOBUL/data.h5 $GLOBUL/Description
-time python globul_ggsn_to_pandas.py $GLOBUL/data.h5 $GLOBUL/OriginalData/ggsn.csv
-time python globul_msc_to_pandas.py $GLOBUL/data.h5 $GLOBUL/OriginalData/msc.csv
