db81a3109c1907ff9ff35094f5f19a503994ecbc
Axel Tidemann <axel.tidemann@gmail.com>
2015-09-02 10:59:37 +0200

    Plotting figures, updating estimates.

--- globul/globul_user_stats.py
+++ globul/globul_user_stats.py
@@ -11,8 +11,10 @@ from collections import defaultdict
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import matplotlib as mpl
+mpl.use('agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 with pd.get_store(sys.argv[1]) as store:
     users = defaultdict(int)
@@ -22,7 +24,21 @@ with pd.get_store(sys.argv[1]) as store:
 
     values = users.values()
 
+with pd.HDFStore('{}_IMSI_count'.format(sys.argv[1]), 'w', complevel=9, complib='blosc') as save:
+    save.append('data', pd.DataFrame(values, columns=['count'], index=[ str(x) for x in users.keys()]), data_columns=True)
+    
     print '{} users, mean: {} std: {} number of datapoints per user.'.format(len(users), np.mean(values), np.std(values))
     sns.distplot(values, kde=False)
     plt.tight_layout()
-    plt.savefig('user_datapoint_distribution.png', dpi=300)
+    plt.savefig('user_datapoint_distribution_nan_included.png', dpi=300)
+
+    plt.clf()
+    nan_number = users[np.nan]
+    del users[np.nan]
+    values = users.values()
+    print '''The NaN group has {} entries. After removal the distribution is:
+    mean: {} std: {} number of datapoints per non-nan user.'''.format(nan_number, np.mean(values), np.std(values))
+    sns.distplot(values, kde=False)
+    plt.tight_layout()
+    plt.savefig('user_datapoint_distribution_nan_excluded.png', dpi=300)
+
