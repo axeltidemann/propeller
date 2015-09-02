4053152921f1146d942516f40523a4c556b5b222
Axel Tidemann <axel.tidemann@gmail.com>
2015-09-02 17:11:29 +0200

    Heatmaps

--- tacos/tacos_csv_to_pandas.py
+++ tacos/tacos_csv_to_pandas.py
@@ -14,13 +14,15 @@ dateparse = lambda x: pd.NaT if pd.isnull(x) else pd.datetime.strptime(x, '%Y-%m
 
 with pd.HDFStore(sys.argv[1], 'a', complevel=9, complib='blosc') as store:
     for input_file in sys.argv[2:]:
-        csv = pd.read_csv(input_file, index_col=0,
-                          parse_dates=[0,1,2], date_parser=dateparse,
-                          dtype={ 'kpafylke': pd.np.object,
-                                  'kpakommune': pd.np.object,
-                                  'fhsseverity': pd.np.float, # If you need to select on these, make them pd.np.object
-                                  'severity': pd.np.float,
-                                  'class': pd.np.float },
+        csv = pd.read_csv(input_file,
+                          index_col=0,
+                          parse_dates=[0,1,2],
+                          date_parser=dateparse,
+                          names=['emsfirsttime','emsclosedtime','actiontime','identifier','node','alarmtype',
+                                 'kpafylke','kpakommune','fhsseverity','severity','networkclass','fhsproblemarea','summary'],
+                          header=0,
+                          converters={'kpafylke': str, 'kpakommune': str,'fhsseverity': str,
+                                      'severity': str, 'class': str}
                           chunksize=50000)
 
         for chunk in csv:
--- tacos/tacos_node_alarm_heatmap.py
+++ tacos/tacos_node_alarm_heatmap.py
@@ -11,10 +11,31 @@ from collections import defaultdict, Counter
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import normalize
+import matplotlib as mpl
+mpl.use('agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 with pd.get_store(sys.argv[1]) as store:
     data = store.select('tacos', columns=['alarmtype', 'node'])
+
+    alarmtypes = data.alarmtype.unique()
+    alarmtypes_index = { alarm: i for i,alarm in enumerate(alarmtypes) }
     
     alarms = defaultdict(lambda : Counter())
-    for name, group in data.groupby(['alarmtype', 'node'], sort=False):
+    for name, group in data.groupby(['node', 'alarmtype'], sort=False):
         alarms[name[0]][name[1]] = len(group)
+
+    heat = np.zeros((len(alarms), len(alarmtypes)))
+
+    for row, node in enumerate(alarms.keys()):
+        for alarmtype in alarms[node].keys():
+            heat[row,alarmtypes_index[alarmtype]] = alarms[node][alarmtype]
+
+
+    heat_norm = normalize(heat, axis=1, norm='l1')
+    sns.heatmap(heat_norm, xticklabels=False, yticklabels=False)
+    plt.xlabel('Alarm types')
+    plt.ylabel('Nodes')
+    plt.savefig('node alarmtypes heat map.png', dpi=300)
