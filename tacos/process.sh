bc4e2f94e3d35568aa8509e5948a5f299186b0dc
Axel Tidemann <axel.tidemann@gmail.com>
2015-09-03 09:02:39 +0200

    Input argument change.

--- tacos/process.sh
+++ tacos/process.sh
@@ -3,4 +3,4 @@
 python tacos_htm_clean.py $DATA/TACOS/tacos*htm
 python tacos_htm_to_csv.py $DATA/TACOS/tacos*cleaned
 python tacos_csv_to_pandas.py $DATA/TACOS/data.h5 $DATA/TACOS/tacos*csv
-python tacos_xlsx_to_pandas.py $DATA/TACOS/*xlsx
+python tacos_xlsx_to_pandas.py $DATA/TACOS/data.h5 $DATA/TACOS/*xlsx
