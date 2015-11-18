cf17ca41987aa1813fb9dfd028a82c8b7dbe5167
Axel Tidemann <axel.tidemann@gmail.com>
2015-11-18 17:13:45 +0100

    Update ami-763a311e.sh

--- misc/ami-763a311e.sh
+++ misc/ami-763a311e.sh
@@ -3,7 +3,7 @@ sudo apt-get -y install emacs24-nox
 sudo apt-get -y install htop
 cd caffe
 sudo pip install -r examples/web_demo/requirements.txt
-./scripts/download_model_binary.py models/bvlc_reference_caffenet
+./scripts/download_model_binary.py models/bvlc_reference_caffenet # Once, this took 1 hour !
 ./data/ilsvrc12/get_ilsvrc_aux.sh
 echo 'PS1="\[\e[33m\]\t \e[35m\e[37m\e[36m\h\e[37m:\e[32m\w\e[0m\n> "' >> .profile
 echo 'export PYTHONPATH=/home/ubuntu/caffe/python/' >> .profile
