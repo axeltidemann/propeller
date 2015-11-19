f0dca73e7b49ada652aab3fc9b632191097a87ac
Axel Tidemann <axel.tidemann@gmail.com>
2015-11-19 17:53:42 +0100

    Update ami-763a311e.sh

--- misc/ami-763a311e.sh
+++ misc/ami-763a311e.sh
@@ -6,5 +6,4 @@ sudo pip install -r examples/web_demo/requirements.txt
 ./scripts/download_model_binary.py models/bvlc_reference_caffenet # Once, this took 1 hour !
 ./data/ilsvrc12/get_ilsvrc_aux.sh
 echo 'PS1="\[\e[33m\]\t \e[35m\e[37m\e[36m\h\e[37m:\e[32m\w\e[0m\n> "' >> .profile
-echo 'export PYTHONPATH=/home/ubuntu/caffe/python/' >> .profile
-# you might need to .profile
+# PYTHONPATH=/home/ubuntu/caffe/python/ MPLBACKEND="Agg" python examples/web_demo/app.py -p 80
