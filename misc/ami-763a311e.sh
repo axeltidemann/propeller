6e389dab83f6bb577ac3630dcb06912a9de7b945
Axel Tidemann <axel.tidemann@gmail.com>
2015-11-20 11:35:19 +0100

    Update ami-763a311e.sh

--- misc/ami-763a311e.sh
+++ misc/ami-763a311e.sh
@@ -6,4 +6,7 @@ sudo pip install -r examples/web_demo/requirements.txt
 ./scripts/download_model_binary.py models/bvlc_reference_caffenet # Once, this took 1 hour !
 ./data/ilsvrc12/get_ilsvrc_aux.sh
 echo 'PS1="\[\e[33m\]\t \e[35m\e[37m\e[36m\h\e[37m:\e[32m\w\e[0m\n> "' >> .profile
-# PYTHONPATH=/home/ubuntu/caffe/python/ MPLBACKEND="Agg" python examples/web_demo/app.py -p 80
+cd /home/ubuntu
+git clone https://github.com/axeltidemann/propeller.git
+cd propeller/web_demos/caffe
+PYTHONPATH=/home/ubuntu/caffe/python/ MPLBACKEND="Agg" python app.py -p 80 # add -g for GPU processing
