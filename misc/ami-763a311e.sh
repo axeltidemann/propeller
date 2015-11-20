7cb3daabe852ba55d06db6ffc9f7e37fdc1e5714
Axel Tidemann <axel.tidemann@gmail.com>
2015-11-20 14:15:19 +0100

    Update ami-763a311e.sh

--- misc/ami-763a311e.sh
+++ misc/ami-763a311e.sh
@@ -10,7 +10,6 @@ cd ../caffe
 pip install -r examples/web_demo/requirements.txt
 ./scripts/download_model_binary.py models/bvlc_reference_caffenet # Once, this took 1 hour !
 ./data/ilsvrc12/get_ilsvrc_aux.sh
-echo 'PS1="\[\e[33m\]\t \e[35m\e[37m\e[36m\h\e[37m:\e[32m\w\e[0m\n> "' >> .profile
 cd /home/ubuntu
 git clone https://github.com/axeltidemann/propeller.git
 cd propeller/web_demos/caffe
