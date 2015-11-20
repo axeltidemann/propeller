e633f362d2d56175606f6dd09c87e6577fcf2448
Axel Tidemann <axel.tidemann@gmail.com>
2015-11-20 12:34:21 +0100

    Update ami-763a311e.sh

--- misc/ami-763a311e.sh
+++ misc/ami-763a311e.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt-get -y install emacs24-nox
-sudo apt-get -y install htop
+apt-get -y install emacs24-nox
+apt-get -y install htop
+pip install redis
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
-sudo make install
+make install
 cd ../caffe
-sudo pip install -r examples/web_demo/requirements.txt
+pip install -r examples/web_demo/requirements.txt
 ./scripts/download_model_binary.py models/bvlc_reference_caffenet # Once, this took 1 hour !
 ./data/ilsvrc12/get_ilsvrc_aux.sh
 echo 'PS1="\[\e[33m\]\t \e[35m\e[37m\e[36m\h\e[37m:\e[32m\w\e[0m\n> "' >> .profile
