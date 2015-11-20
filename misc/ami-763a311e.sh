7604dea9177fe7599cd23d41f52bbcf3f34da1c7
Axel Tidemann <axel.tidemann@gmail.com>
2015-11-20 11:36:23 +0100

    Update ami-763a311e.sh

--- misc/ami-763a311e.sh
+++ misc/ami-763a311e.sh
@@ -9,4 +9,4 @@ echo 'PS1="\[\e[33m\]\t \e[35m\e[37m\e[36m\h\e[37m:\e[32m\w\e[0m\n> "' >> .profi
 cd /home/ubuntu
 git clone https://github.com/axeltidemann/propeller.git
 cd propeller/web_demos/caffe
-PYTHONPATH=/home/ubuntu/caffe/python/ MPLBACKEND="Agg" python app.py -p 80 # add -g for GPU processing
+nohup PYTHONPATH=/home/ubuntu/caffe/python/ MPLBACKEND="Agg" python app.py -p 80 # add -g for GPU processing
