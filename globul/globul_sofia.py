2cc4997272928b78051927c8dd353a042c137760
Axel Tidemann <axel.tidemann@gmail.com>
2016-03-04 14:52:39 +0100

    More smooth Sofia data extraction.

--- globul/globul_sofia.py
+++ globul/globul_sofia.py
@@ -1,47 +1,47 @@
 '''
-Extracts outgoing call/SMS data for Sofia.
-
-python globul_sofia.py /path/to/meta.h5 /path/to/msc.h5
+Creates a subset of the Bulgaria file that is only for the site IDs in "SOFIA_CITY".
 
 Author: Axel.Tidemann@telenor.com
 '''
 
-import sys
+import argparse
 
 import pandas as pd
 import numpy as np
 from keras.utils import generic_utils
 
-meta_path = sys.argv[1]
-msc_path = sys.argv[2]
-sofia_path = '{}sofia.h5'.format(meta_path[:meta_path.rfind('/')+1] if meta_path.rfind('/') > -1 else '')
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '--name',
+    help='Name of the created file HDF5 file.',
+    default='sofia.h5')
+parser.add_argument(
+    '--meta',
+    help='Path to the meta HDF5 file')
+parser.add_argument(
+    '--source',
+    help='Where bulgaria.h5 resides')
+parser.add_argument(
+    '--chunksize',
+    help='Chunk size to iterate over HDF5 file',
+    type=int,
+    default=50000) 
+args = parser.parse_args()
 
-with pd.get_store(meta_path) as meta_store, \
-     pd.get_store(msc_path) as msc_store, \
-     pd.HDFStore(sofia_path, 'w', complevel=9, complib='blosc') as sofia_store:
-
-    chunksize = int(5e5)
+with pd.get_store(args.meta) as meta_store, \
+     pd.get_store(args.source) as data_store, \
+     pd.HDFStore(args.name, 'w', complevel=9, complib='blosc') as sofia_store:
 
     site_info = meta_store['site_info']
     sofia = site_info.query("Region == 'SOFIA_CITY'")
 
-    progbar = generic_utils.Progbar(msc_store.get_storer('data').nrows)
-
-    print 'Extracting outgoing calls/SMS from Sofia'
+    progbar = generic_utils.Progbar(data_store.get_storer('data').nrows)
     
-    for chunk in msc_store.select('data', chunksize=chunksize):
-        chunk['outgoing'] = chunk.typeCode.apply(lambda x: x in ['01', '05'])
-        chunk = pd.DataFrame(chunk.query('outgoing == True')) # Cannot do assignments on slices later on
-
-        cell_ID_truncated = chunk.cell_ID.str[:-1]
-        in_sofia = cell_ID_truncated.apply(lambda x: len(x) and (int(x) in sofia.index))
-        chunk['outgoing_sofia'] = chunk.outgoing == in_sofia
-        chunk = chunk.query('outgoing_sofia == True')
-        del chunk['outgoing']
-        del chunk['outgoing_sofia']
-        del chunk['calledSubscriberIMSI']
+    for chunk in data_store.select('data', chunksize=args.chunksize):
+        chunk['in_sofia'] = chunk.site_ID.apply(lambda x: int(x) in sofia.index)
+        chunk = chunk.query('in_sofia == True')
+        del chunk['in_sofia']
         sofia_store.append('data', chunk, data_columns=True)
-
-        progbar.add(chunksize)
+        progbar.add(args.chunksize)
         
-    print 'Sofia users stored in {}'.format(sofia_path)
+    print 'Sofia users stored in {}'.format(args.name)
