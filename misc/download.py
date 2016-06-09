eabfd4cffbebad1865866a3a7177a3936b9292b5
Axel Tidemann <axel.tidemann@gmail.com>
2016-06-09 14:46:50 +0000

    resuming downloads possible

--- misc/download.py
+++ misc/download.py
@@ -9,7 +9,6 @@ import glob
 import multiprocessing as mp
 import argparse
 import cStringIO as StringIO
-import os
 import sys
 import uuid
 import logging
@@ -25,12 +24,12 @@ parser.add_argument(
     help='Folder with URL text files')
 parser.add_argument(
     'target',
-    help='Where to put the images, a folder will be created for URL text file.')
+    help='Where to put the images, a folder will be created for each category.')
 parser.add_argument(
     '--limit',
     help='Limit the number of files to download',
-    type=float,
-    default=np.inf)
+    type=int,
+    default=10000)
 args = parser.parse_args()
 
 def save_to_jpg(data, folder_name):
@@ -42,25 +41,40 @@ def save_to_jpg(data, folder_name):
         img.save(filename=filename)
             
 def get(target_folder, limit, source_filename):
+    
     print 'Downloading from {}'.format(source_filename)
+    category_name = os.path.basename(source_filename).replace('.txt', '')
     category_folder = '{}/{}'.format(target_folder, os.path.basename(os.path.normpath(source_filename)).replace('.txt',''))
     if not os.path.exists(category_folder):
         os.makedirs(category_folder)
-    with open(source_filename) as _file:
-        i = 0
-        for line in _file:
-            try:
-                response = requests.get(line.rstrip(), timeout=10)
-                save_to_jpg(response.content, category_folder)
-                i += 1
 
-                if i == limit:
-                    break
+    already_stored = len(os.listdir(category_folder))
+    print 'Category {} has {} files, aiming for {} more'.format(category_name, already_stored, limit - already_stored)
+
+    if already_stored == limit:
+        return
+    
+    with open(source_filename) as _file:
+        download_counter = already_stored
+        line_counter = 0
+        
+        for line in _file:
+            if line_counter >= already_stored:
+                try:
+                    response = requests.get(line.rstrip(), timeout=10)
+                    save_to_jpg(response.content, category_folder)
                     
-            except Exception as e:
-                pass
+                    download_counter += 1
+
+                    if download_counter == limit:
+                        break
+
+                except Exception as e:
+                    pass
+                    
+            line_counter += 1
                 
-    print '{} done, {} files downloaded.'.format(source_filename, i)
+    print 'Category {} done, {} files downloaded, {} in total.'.format(category_name, download_counter - already_stored, download_counter)
 
 pool = mp.Pool()
 par_get = partial(get, args.target, args.limit)
