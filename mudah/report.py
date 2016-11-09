69d9d73a87adde8b7633d5fa0e8965b76403c1dd
Axel Tidemann <axel.tidemann@gmail.com>
2016-11-09 08:29:48 +0000

    Made local plots possible

--- mudah/report.py
+++ mudah/report.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../training')))
 
 import plotly
+import plotly.graph_objs as go
 
 from transfer_stats import evaluate
 
@@ -46,18 +47,88 @@ html = '''
 <head>
 <title>Mudah report on classifier performance</title>
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+<style>
+img{
+  height: 300px;
+}
+
+th{
+  text-align: left;
+}
+</style>
+
+
 </head>
 <body>
+
+<table>
 '''
 
 for model in args.models:
-    figure, accuracy, top_k_accuracy = evaluate(model, h5_files, args.top_k, categories)
-    div = plotly.offline.plot_mpl(figure, show_link=False, auto_open=False,
-                                  output_type='div', include_plotlyjs=False)
+    plotly_data, accuracy, top_k_accuracy, stats = evaluate(model, h5_files, args.top_k, categories)
 
-    html += '{}<br>accuracy: {}. top {} accuracy: {}.<br>{}<br>'.format(os.path.basename(model),
-                                                                        accuracy, args.top_k, top_k_accuracy, div)
-html += '''
+    layout= go.Layout(
+        showlegend=False,
+        hovermode='closest')
+
+    figure = go.Figure(data=plotly_data, layout=layout)
+
+    from plotly.offline.offline import _plot_html
+
+    plot_html, plotdivid, width, height = _plot_html(figure, False, '', True, '100%', '100%', False)
+    
+    html += '<tr><td colspan=2><h3>{}</h3>accuracy: {} top-{} accuracy: {}</td></tr><tr><td>{}</td>'.format(os.path.basename(model),
+                                                                                                       accuracy, args.top_k,
+                                                                                                       top_k_accuracy, plot_html)
+
+    html += '''
+    <td>
+    <img id="hover-image-'''+str(plotdivid)+'''" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">
+    <div id="hoverinfo-'''+str(plotdivid)+'''"></div>
+    </td>
+    </tr>
+
+    <tr><td colspan=2>
+
+    <table width=800>
+    <tr><th>Category</th><th>Accuracy</th><th>Top-'''+str(args.top_k)+''' accuracy</th><th>Correct confidence</th></tr>'''
+    
+    for row in sorted(stats, key=lambda x: x[1], reverse=True):
+        html += '<tr>'
+        for element in row:
+            html += '<td>{}</td>'.format(element)
+        html += '</tr>'
+    html += '''
+    </table>
+    
+    </td>
+    </tr>
+    
+    <script>
+    var myPlot = document.getElementById("''' + str(plotdivid) + '''"),
+    hoverInfo = document.getElementById("hoverinfo"'''+str(plotdivid)+'''"),
+    hoverImage = document.getElementById("hover-image"'''+str(plotdivid)'''+");
+
+    myPlot.on('plotly_hover', function(data){
+        data.points.map(function(d){
+           var info=JSON.parse(d.data.text[d.pointNumber]);
+           hoverImage.src="http://52.77.195.194:8080/static"+info.path;
+           if( d.data.name == info.prediction ){
+               hoverInfo.innerHTML = 'Correctly classified as <b>' + info.prediction +'</b>, score <b>' + d.y.toPrecision(3) + '</b>';
+           } else {
+               hoverInfo.innerHTML = 'Was in the <b>'+d.data.name+'</b> category, wrongly classified as <b>'+info.prediction+'</b> with score <b>' + d.y.toPrecision(3) + '</b>';
+           }
+        });
+    });
+
+    </script>
+    
+    '''
+    
+html+='''
+</table>
+
 </body>
 </html>
 '''
--- training/transfer_stats.py
+++ training/transfer_stats.py
@@ -9,27 +9,25 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../misc')))
 from itertools import chain
+import json
 
 import pandas as pd
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 from tensorflow.python.framework import ops
 import numpy as np
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
+import ipdb 
+
+import plotly.graph_objs as go
+
 
 from training_data import states
-from utils import load_graph, pretty_float
+from utils import load_graph, pretty_float as pf
 
 def evaluate(model, h5_files, top_k, categories=None):
-    
-    figure = plt.figure()
-    
-    colormap = plt.cm.spectral
-    colors = [colormap(i) for i in np.linspace(0, 1, len(h5_files))]
-    colors = chain(*zip(colors, colors))
-    plt.gca().set_color_cycle(colors)
+    h5_files = sorted(h5_files)
+
+    plotly_data = []
     
     with tf.Session() as sess:
         print('Evaluating {}'.format(model))
@@ -42,72 +40,88 @@ def evaluate(model, h5_files, top_k, categories=None):
 
         all_accuracies = []
         all_top_k_accuracy = []
+        stats = []
 
-        for i, h5 in enumerate(h5_files):
+        for target, h5 in enumerate(h5_files):
 
-            data, _ = states([ h5 ])
+            data = pd.read_hdf(h5)
 
-            X,_ = data
+            predictions = sess.run(transfer_predictor, { 'input:0': data })
 
-            Y = np.zeros((len(X), len(h5_files)))
-            Y[:,i] = 1
-
-            predictions = sess.run(transfer_predictor, { 'input:0': X })
-
-            correct = np.argmax(predictions, axis=1) == np.argmax(Y, axis=1)
+            correct = np.argmax(predictions, axis=1) == target
             accuracy = np.mean(correct)
 
-            top_k_accuracy = np.mean([ np.argmax(target) in np.argsort(prediction)[-top_k:]
-                                       for target, prediction in zip(Y, predictions) ])
+            top_k_accuracy = np.mean([ target in np.argsort(prediction)[-top_k:]
+                                       for prediction in predictions ])
 
-            correct_scores = np.max(predictions[np.where(correct)], axis=1)
+            correct_scores = np.max(predictions[correct], axis=1)
             correct_x = np.linspace(0,1, num=len(correct_scores))
             correct_confidence = np.mean(correct_scores)
-            correct_confidence_median = np.median(np.max(predictions[np.where(correct)], axis=1))
-            correct_confidence_std = np.std(np.max(predictions[np.where(correct)], axis=1))
+            correct_confidence_median = np.median(np.max(predictions[correct], axis=1))
+            correct_confidence_std = np.std(np.max(predictions[correct], axis=1))
 
             category_i = os.path.basename(h5).replace('.h5','')
             category = categories[category_i]['name'] if categories else category_i
-            plt.plot(correct_x, sorted(correct_scores), label=category)
 
-            wrong_scores = np.max(predictions[np.where(~correct)], axis=1)
+            plotly_data.append(go.Scatter(
+                x=correct_x,
+                y=sorted(correct_scores),
+                mode='lines',
+                name=category,
+                hoverinfo='name+y',
+                text=[ json.dumps({ 'path': path, 'prediction': category })
+                       for path in data.index[correct]]))
+            
+            wrong_scores = np.max(predictions[~correct], axis=1)
+            wrong_categories_i = np.argmax(predictions[~correct], axis=1)
             wrong_x = np.linspace(0,1, num=len(wrong_scores))
             wrong_confidence = np.mean(wrong_scores)
-            wrong_confidence_median = np.median(np.max(predictions[np.where(~correct)], axis=1))
-            wrong_confidence_std = np.std(np.max(predictions[np.where(~correct)], axis=1))
+            wrong_confidence_median = np.median(np.max(predictions[~correct], axis=1))
+            wrong_confidence_std = np.std(np.max(predictions[~correct], axis=1))
 
-            plt.plot(wrong_x, sorted(wrong_scores), '--', label=category)
+            wrong_categories = []
+
+            for i in wrong_categories_i:
+                wrong_i = os.path.basename(h5_files[i]).replace('.h5','')
+                wrong_categories.append(categories[wrong_i]['name'] if categories else wrong_i)
+
+            plotly_data.append(go.Scatter(
+                x=wrong_x,
+                y=sorted(wrong_scores),
+                mode='markers',
+                name=category,
+                hoverinfo='name+y',
+                text=[ json.dumps({ 'path': path, 'prediction': prediction }) for path, prediction in
+                       zip(data.index[~correct], wrong_categories)]))
             
             print('Category {}, {} images. \t accuracy: {} top_{} accuracy: {} '
                   'correct confidence: {}, {}, {} wrong confidence: {}, {}, {} '
                   'diff: {}, {}, {}'
-                  ''.format(category_i, len(X), pretty_float(accuracy), top_k,
-                            pretty_float(top_k_accuracy),
-                            pretty_float(correct_confidence),
-                            pretty_float(correct_confidence_median),
-                            pretty_float(correct_confidence_std),
-                            pretty_float(wrong_confidence),
-                            pretty_float(wrong_confidence_median),
-                            pretty_float(wrong_confidence_std),
-                            pretty_float(correct_confidence - wrong_confidence),
-                            pretty_float(correct_confidence_median - wrong_confidence_median),
-                            pretty_float(correct_confidence_std - wrong_confidence_std)
+                  ''.format(category_i, len(data), pf(accuracy), top_k,
+                            pf(top_k_accuracy),
+                            pf(correct_confidence),
+                            pf(correct_confidence_median),
+                            pf(correct_confidence_std),
+                            pf(wrong_confidence),
+                            pf(wrong_confidence_median),
+                            pf(wrong_confidence_std),
+                            pf(correct_confidence - wrong_confidence),
+                            pf(correct_confidence_median - wrong_confidence_median),
+                            pf(correct_confidence_std - wrong_confidence_std)
                         ))
 
             all_accuracies.append(accuracy)
             all_top_k_accuracy.append(top_k_accuracy)
+            stats.append([ category, pf(accuracy), pf(top_k_accuracy), pf(correct_confidence) ])
 
-
-        mean_accuracy = np.mean(all_accuracies)
-        top_k_accuracy = np.mean(all_top_k_accuracy)
-        print('Average accuracy across categories: {}'.format(pretty_float(mean_accuracy)))
-        print('Average top_{} accuracy across categories: {}'.format(top_k, pretty_float(top_k_accuracy)))
+        mean_accuracy = pf(np.mean(all_accuracies))
+        top_k_accuracy = pf(np.mean(all_top_k_accuracy))
+        print('Average accuracy across categories: {}'.format(mean_accuracy))
+        print('Average top_{} accuracy across categories: {}'.format(top_k, top_k_accuracy))
 
     tf.reset_default_graph()
 
-    plt.ylim([0,1])
-
-    return figure, mean_accuracy, top_k_accuracy
+    return plotly_data, mean_accuracy, top_k_accuracy, stats
 
 if __name__ == "__main__":
 
