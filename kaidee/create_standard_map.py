a2e0933aa9cdcc342566c12b0017e4122560dd97
Axel Tidemann <axel.tidemann@gmail.com>
2016-11-15 12:02:29 +0700

    Going towards unified category structure. This is a good thing.

---
+++ kaidee/create_standard_map.py
@@ -0,0 +1,44 @@
+# Copyright 2016 Telenor ASA, Author: Axel Tidemann
+
+'''
+Reads the provided Excel file and maps it into JSON 
+hierarchy that will eventually evolve to become the standard.
+'''
+
+import argparse
+import glob
+import os
+import json
+
+from xlrd import open_workbook
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    'excel_file',
+    help='Excel file with categories')
+parser.add_argument(
+    '--categories_filename',
+    help='Name of JSON file', 
+    default='categories.json')
+args = parser.parse_args()
+
+book = open_workbook(args.excel_file)
+sheet = book.sheet_by_index(0)
+
+category_level = map(lambda x: str(int(x)), sheet.col_values(6, start_rowx=1))
+
+top_ids = map(lambda x: str(int(x)), sheet.col_values(0, start_rowx=1))
+top_names = sheet.col_values(1, start_rowx=1)
+
+sub_ids = map(lambda x: str(int(x)), sheet.col_values(4, start_rowx=1))
+sub_names = sheet.col_values(5, start_rowx=1)
+
+categories = {}
+for top_i, top_n, sub_i, sub_n, lvl in zip(top_ids, top_names, sub_ids, sub_names, category_level):
+    if lvl == 1:
+        categories[top_i] = {"name": n, "level":lvl }
+    else:
+        categories[sub_i] = {"name": sub_n, "level": lvl, "parent": top_i}
+
+with open(args.categories_filename, 'w') as _file:
+    json.dump(categories, _file, sort_keys=True, indent=4)
